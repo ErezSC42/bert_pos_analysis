9e05d2add7f9afcf3b8df96038f64a083ac66238
erez <erezshwarts1@msn.com>
2020-09-29 21:22:30 +0300

    added feature importance tables

--- build_env.sh
+++ build_env.sh
@@ -1,7 +1,9 @@
 mkdir "data"
-#curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3226{/ud-treebanks-v2.6.tgz,/ud-documentation-v2.6.tgz,/ud-tools-v2.6.tgz}
+mkdir "confusion_matrices"
+mkdir "images"
+mkdir "bert_embeddings"
+
 cd "data"
-#curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1743{/deltacorpus-1.1.tar}
 
 # download data
 wget 'http://archive.aueb.gr:8085/files/en_partut-ud-train.conllu' 'en_partut-ud-train.conllu.txt'
